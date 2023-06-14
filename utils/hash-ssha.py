60e7823e66c64c1a46949c4ca8633d3837c00e75
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2023-06-14 15:19:09 +0200

    feat: Add utility script to generate SSHA hashes

---
+++ utils/hash-ssha.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Generate a SSHA hash according to RFC 2307
+
+import base64
+import hashlib
+import os
+import sys
+
+password = sys.argv[1]
+
+h = hashlib.sha1(password.encode("utf-8"))
+salt = os.urandom(4)
+h.update(salt)
+
+print("{SSHA}" + base64.b64encode(h.digest() + salt).decode("utf-8"), end="")
